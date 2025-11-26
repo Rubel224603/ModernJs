d9f011113463f59ea14af9714301d96b65357210
Rubel224603 <cserubelhosen@gmail.com>
2025-11-26 19:43:19 +0600

    task

--- Problem/array/arrayToObjectConvert.js
+++ Problem/array/arrayToObjectConvert.js
@@ -21,3 +21,24 @@ function arrayToObjectConvert (){
    
 }
 arrayToObjectConvert();
+
+//array method;
+
+const days = ["sta","sun","mon","tues","wed","april"];
+
+const sortedDay = days.toSorted();
+console.log("Sorted array:",sortedDay);
+console.log("The original Array:",days);
+console.log("toSorted Method Immutable");
+const reversDay = sortedDay.toReversed();
+console.log("Reverse Array:",reversDay);
+
+const spilceDay  = reversDay.toSpliced(2,2);
+console.log("Remove Some elements:",spilceDay);
+console.log(reversDay);
+
+//only sort method...
+const numbers= [10,30,3,30,20,100];
+console.log("Unsorted Array:",numbers);
+numbers.sort((a,b)=>{return b-a});
+console.log("Sorted array:",numbers);
---
+++ Problem/array/static.js
@@ -0,0 +1,73 @@
+//check array method static or not...
+
+const numbres= [1,3,4,5,6,6];
+Array.isArray(1);
+console.log(Array.isArray(numbres));
+
+const newArray  = Array.of(1,2,4,55);
+console.log(newArray);
+
+//create array useing the constructor with 5  elements;
+
+const cars = new Array("BMW","Toyoto","Marcidus","ff",'eee');
+console.log(Array.isArray(cars));
+
+// create an array of  3 empty slot...
+ 
+const flowers = new Array(3);
+console.log(flowers);
+//create array 6 elements useing  array litarel  and access 4 element useing length property;
+
+
+const colors = ['red','green','white',"black","yellow","brown"];
+const length = colors.length;
+console.log(length);
+console.log(colors[length-3]);
+
+// filter employees who work in the "Engineering" department?
+
+const employees = [
+  { id: 1, name: "Alice", departmentId: 1, salary: 5000 },
+  { id: 2, name: "Bob", departmentId: 2, salary: 7000 },
+  { id: 3, name: "Charlie", departmentId: 3, salary: 4500 },
+  { id: 4, name: "Diana", departmentId: 1, salary: 5500 },
+  { id: 5, name: "Edward", departmentId: 2, salary: 8000 },
+  { id: 6, name: "Fiona", departmentId: 4, salary: 6000 },
+  { id: 7, name: "George", departmentId: 3, salary: 5200 },
+  { id: 8, name: "Helen", departmentId: 4, salary: 7200 },
+  { id: 9, name: "Ian", departmentId: 2, salary: 4800 },
+  { id: 10, name: "Jane", departmentId: 1, salary: 5100 },
+];
+
+const departments = [
+  { id: 1, name: "HR" },
+  { id: 2, name: "Engineering" },
+  { id: 3, name: "Marketing" },
+  { id: 4, name: "Sales" },
+];
+
+
+const departmentLength = departments.length ;
+const employeLength = employees.length; 
+
+console.log("Department Length: ", departmentLength); 
+console.log("Employees Length: ", employeLength); 
+
+
+function getEngineers(){
+    for(let i= 0;i<departmentLength;i++){
+        //console.log(departments[i]);
+        if(departments[i].id === departments[1].id){
+            console.log("Department Id: = ", departments[i].id);
+            let engineeringId = departments[i].id;
+            console.log("Department Name: = ", departments[i].name);
+            for(let j= 0; j<employeLength;j++){
+               if(employees[j].departmentId === engineeringId){
+                console.log(employees[j].name);
+               };
+            }
+        }
+    }
+}
+
+getEngineers();
