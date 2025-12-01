83689a953115819437f17686a209101adbc992eb
Rubel224603 <cserubelhosen@gmail.com>
2025-12-01 18:22:38 +0600

    task

---
+++ Problem/array/task37.js
@@ -0,0 +1,30 @@
+//Assume each employee can have multiple skills. Create an array of employee skills and flatten them.
+// Example: [{name: "Alice", skills: ["Excel", "Management"]}, ...].
+const employees = [
+  { id: 1, name: "Alice", departmentId: 1, salary: 5000 },
+  { id: 2, name: "Bob", departmentId: 2, salary: 7000 },
+  { id: 3, name: "Charlie", departmentId: 3, salary: 4500 },
+  { id: 4, name: "Diana", departmentId: 1, salary: 5500 },
+  { id: 5, name: "Edward", departmentId: 2, salary: 8000 },
+  { id: 6, name: "Fiona", departmentId: 4, salary: 6000 },
+  { id: 7, name: "George", departmentId: 3, salary: 5200 },
+  { id: 8, name: "Helen", departmentId: 4, salary: 7200 },
+  { id: 9, name: "Ian", departmentId: 2, salary: 4800 },
+  { id: 10, name: "Jane", departmentId: 1, salary: 5100 },
+];
+
+//first insert new property in each object 
+
+const newf = employees.forEach((emp)=>{
+    emp.skill="Web";
+
+});
+console.log(newf);
+
+  //console.log(employees);
+
+  const newProperty = employees.map((emp)=>{
+    return {...emp, skill: "Web"}
+
+    });
+  console.log(newProperty);
