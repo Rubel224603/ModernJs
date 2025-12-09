f86c6bdbee005138c113c581c18ae6a4b44f5816
Rubel224603 <cserubelhosen@gmail.com>
2025-12-09 18:23:14 +0600

    js empy array check

--- Problem/array/emptyArray.js
+++ Problem/array/emptyArray.js
@@ -15,3 +15,10 @@ console.log(cars);
 cars.length = 0;
 console.log(cars);
 //  Create an Array of 10 elements. Use the splice() method to empty the array.
+
+//what happend if two empty array concatenate
+let con = []+[];
+console.log("empty Array: ", ([]+[]));
+console.log("Concatenate Array type : ",typeof con);
+console.log("Concatenate length:", con.length);
+
