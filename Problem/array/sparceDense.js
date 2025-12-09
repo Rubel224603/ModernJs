74c15578b066c06cea09adb1c8da13c91c81fd21
Rubel224603 <cserubelhosen@gmail.com>
2025-12-09 19:30:12 +0600

    js

--- Problem/array/sparceDense.js
+++ Problem/array/sparceDense.js
@@ -9,7 +9,9 @@ console.log(dens);
 
 const hasHoles = Object.keys(sparArray).length !== sparArray.length;
 
-console.log("Yes, empty slot hase available: ",hasHoles);
+console.log("The array has empty element: ",hasHoles);
+const checkDenseArry = dens.length === Object.keys(dens).length;
+console.log("Dense array:= ",checkDenseArry);
 
 let fil= sparArray.fill(1,3);
 console.log(fil);
